48a09f4e6781e18a286b8111058bcc5c0798fa08
claudiareyesjofre <claudia.reyes.joffre@gmail.com>
2023-02-23 15:25:39 +0000

    listoarreglado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,75 +1,83 @@
-import React,{useState,useEffect} from "react";
 
-const Home = () => {
-	const [tarea, nueva] = useState([]);
-	const borrar = (b) => {
-		nueva(tarea.filter((value, index, arr) => {
-			return index != b;
-		  })
-		);
-	  };
-	
+import React, { useState, useEffect } from "react";
 
-function api ()  {
-	var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+const Todo = () => {
+    const [tareas, setTarea] = useState([]);
+    const deleteTarea = (i) => {
+        setTarea(
+            tareas.filter((value, index) => {
+                return index != i;
+            })
+        );
+    };
 
-var raw = JSON.stringify(tarea);
+    function putApi() {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-var requestOptions = {
-  method: 'PUT',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
+        var raw = JSON.stringify(tareas);
+
+        var requestOptions = {
+            method: "PUT",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow",
+        };
+
+        fetch(
+            "https://assets.breatheco.de/apis/fake/todos/user/claudia",
+            requestOptions
+        )
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.log("error", error));
+    }
+
+    useEffect(() => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/claudia")
+            .then((respuesta) => respuesta.json())
+            .then((data) => setTarea(data));
+    }, []);
+
+    useEffect(() => {
+        putApi();
+    }, [tareas])
+
+    return (
+        <div className="container">
+            <h1>Todo List</h1>
+            <input
+                placeholder="Escribir tarea"
+                onKeyPress={(event) => {
+                    if (event.key == "Enter") {
+                        setTarea([...tareas, { label: event.target.value, done: false }]);
+                        event.target.value = "";
+                    }
+                }}
+            />
+
+            {tareas.map((value, index, i) => {
+                return (
+                    <div className="task-container d-flex">
+                        <div className="task-name" key={index}>
+                            {value.label}
+                        </div>
+                        <div className="button-container">
+                            <button
+                                className="delete-button"
+                                onClick={() => deleteTarea(index)}
+                            >
+                                X
+                            </button>
+                        </div>
+                    </div>
+                );
+            })}
+            <div className="left">
+        	    <p>Faltan {tareas.length} por finalizar.</p>
+    	    </div>
+        </div>
+    );
 };
 
-fetch(
-	"http://assets.breatheco.de/apis/fake/todos/user/claudia", 
-	requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log('error', error));
-}
-	useEffect(()=>{
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/claudia")
-		.then((response) => response.json())
-		.then((data) => nueva(data));
-	}, []);
-	
-	useEffect(()=>{
-		api();
-	},[tarea]);
-
-
-
-	return (
-		<div className="container">
-			<h1>Lista de Tareas</h1>
-			<form
-        onSubmit={(event) => {
-          event.preventDefault(); //que no se actualize el componente//
-          nueva([...tarea, { label: event.target[0].value, done: false }]);
-          putApi();
-          event.target[0].value="";
-        }}
-      >
-        
-        <input type="text" placeholder="ingresa"></input>
-        <button> Ingresar </button>
-      </form>
-      {tarea.map((value, index) => {
-        return (
-          <li key={index}>
-            {value.label}
-            <button onClick={() => borrar(index)}> X </button>
-          </li>
-        );
-      })}
-				<div className="left"> {tarea.length} te quedan tareas</div>
-	
-	</div>
-	);
-}; 
-
-
-export default Home;
+export default Todo;
